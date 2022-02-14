5f2e2ec2d6ce42a45efd99fbb635735c921604e2
NateWalchenbach <natewalchenbach24@gmail.com>
2022-02-14 13:08:03 -0800

    Defining Columns

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,31 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // Define Columns Here
+    id: {
+      // Model atributes
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    category_name: {
+
+
+    }
+    }
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
--- models/product.js
+++ models/product.js
@@ -1,7 +1,7 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -10,13 +10,17 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    // id INT NOT NULL PRIMARY KEY AUTO_INCREMENT
+    // product_name VARCHAR NOT NULL
+    // price DECIMAL NOT NULL (VALIDATE???)
+    // stock INT NOT NULL (VALIDATE???)
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
